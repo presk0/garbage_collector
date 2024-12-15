3d60486cafcae3d0cb08993847c8e87ecab4638b
Nicolas Dionis <nidionis@k1r4p6.42mulhouse.fr>
2024-12-15 20:26:28 +0100

    heaader modified

--- include/libgc.h
+++ include/libgc.h
@@ -6,5 +6,4 @@ void    *gc_append(t_list **gc_addr, void *ptr);
 void    clean_exit(t_list **gc_addr);
 void    *gc_calloc(t_list **gc_addr, size_t count, size_t size);
 void    gc_free_item(t_list **gc_addr, void *ptr);
-
 # endif
